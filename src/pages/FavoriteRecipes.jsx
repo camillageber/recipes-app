d817315d1c1079caaade2d79ec36b10827f4544f
kaduh15 <kadu.silva2014@gmail.com>
2022-07-24 11:58:32 -0300

    renderiza todas as receitas favoritadas

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 export default function FavoriteRecipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  console.log(recipes);
+  useEffect(() => {
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (!favoriteRecipes) {
+      setRecipes([]);
+    } else {
+      setRecipes(favoriteRecipes);
+    }
+  }, []);
+
   return (
     <>
-      <Header pageTitle="Favorite Recipes" />
-      <h1>FavoriteRecipes</h1>
+      <Header pageTitle="Favorite Recipes" color="red" />
+      <div className="recipes">
+        {recipes.length > 0 && recipes.map((recipe) => (
+          <p key={ recipe.id }>
+            {recipe.name}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
