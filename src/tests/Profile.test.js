1f8d14c2f3c5bc3827e8787a18acb18200c1a6f3
Camilla Geber <camilla.geber@gmail.com>
2022-07-27 15:51:07 -0300

    Sobe novamente as mudanças nos testes Profile para passar nos requisitos 57 a 62 e não quebrar o 7, 8, 9 e 17

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,29 +1,61 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import Profile from '../pages/Profile';
+import App from '../App';
 
 describe('Testa a tela Profile', () => {
-  test('Verifica se há o título "Profile" na tela', () => {
-    renderWithRouter(<Profile />)
-    const titleEl = screen.getByRole('heading', { name: /profile/i, level: 1 });
+  beforeEach(() => {
+    localStorage.setItem('user', JSON.stringify({ email: 'teste@teste.com' }));
+  })
+  test('Verifica se há o título "Profile" na tela', async () => {   
+    renderWithRouter(<App />, '/profile');
+    localStorage.setItem('user', JSON.stringify({ email: 'teste@teste.com' }));
+    
+    const titleEl = await screen.findByRole('heading', { name: /profile/i, level: 1 });
     expect(titleEl).toBeInTheDocument();
   });
-  test('Verifica se há o botão "Done Recipes" na tela e redireciona para a página correta', () => {
-    const {history} = renderWithRouter(<Profile />)
-    const btnDoneRecipes = screen.getByRole('button', { name: /done recipes/i });
+
+  test('Verifica se há o botão "Done Recipes" na tela e redireciona para a página correta', async () => {
+    const {history} = renderWithRouter(<App />, '/profile');
+
+    const btnDoneRecipes = await screen.findByRole('button', { name: /done recipes/i });
     expect(btnDoneRecipes).toBeInTheDocument();
     userEvent.click(btnDoneRecipes);
     const path = history.location.pathname;
-    console.log(path)
-    history.push('/done-recipes')
+    // console.log(path)
     expect(path).toEqual('/done-recipes')
   });
-  test('testa botão Done', () => {
-    // acessar os elementos da tela
-    renderWithRouter(<Profile />)
-    const buttonDone = screen.getByTestId('profile-done-btn');
+
+  test('Testa botão Done Recipes', async () => {
+   renderWithRouter(<App />, '/profile');
+
+    const buttonDone = await screen.findByTestId('profile-done-btn');
     userEvent.click(buttonDone);
+
+    const titleEl = await screen.findByRole('heading', { name: /donerecipes/i})
+    expect(titleEl).toBeInTheDocument();
   });
+
+  test('Testa botão Favorite Recipes', async () => {
+  renderWithRouter(<App />, '/profile');
+
+    const buttonDone = await screen.findByTestId('profile-favorite-btn');
+    userEvent.click(buttonDone);
+
+    const titleEl = await screen.findByRole('heading', { name: /favoriterecipes/i})
+    expect(titleEl).toBeInTheDocument();
+  });
+
+  test('Testa o botão "Logout"', async () => {
+  renderWithRouter(<App />, '/profile');
+       
+    const buttonLogout = await screen.findByTestId("profile-logout-btn");
+    userEvent.click(buttonLogout);
+    const cleanedLocalStorage = localStorage.getItem('user');
+    expect(cleanedLocalStorage).toBe(null);
+    const returnToLogin = await screen.findByTestId('email-input');
+    expect(returnToLogin).toBeInTheDocument();
+})
+
 });
